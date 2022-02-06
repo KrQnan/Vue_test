b2807c957de97f5851654a17bc21449a32f38268
Kevin Ramlinger <Kevin023@live.se>
2022-02-07 00:40:29 +0100

    First test vue project

---
+++ vue_test/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Card.vue'
+import Card from './components/Card.vue'
+</script>
+
+<template>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+    <Card></Card>
+    <Card></Card>
+  </div>
+</body>
+</html>
+  
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.container{
+  display: flex;
+}
+
+</style>
---
+++ vue_test/src/components/Card.vue
@@ -0,0 +1,66 @@
+<script>
+import { ref } from "vue";
+</script>
+
+<template>
+        <div id="card">
+            <h1>Random object</h1>
+            <img src="https://picsum.photos/200/300?random=10" alt="">
+            <div class="overlay">
+                <h5 id="text">Check this</h5>
+            </div>
+        </div>
+</template>
+
+<style>
+#card{
+    position: relative;
+    width:18rem
+    
+}
+#card img{
+    width: 100%;
+    height: auto;
+}
+
+.overlay{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: .5s ease;
+    background-color: rgba(176, 177, 169, 0.5);
+}
+#card:hover .overlay{
+    opacity: 1
+}
+
+#card:hover{
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    opacity: 0.5;
+    transition: .9s ease;
+    background-color: rgba(176, 177, 169, 0.5);
+}
+
+
+#text{
+    color: white;
+    font-size: 20px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+
+
+</style>
